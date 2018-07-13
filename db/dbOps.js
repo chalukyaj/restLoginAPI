519a211bb84da540698aaef1d96cd06aab129a95
Chalukya <chalukya.j@stanplus.co.in>
2018-07-13 12:11:27 +0530

    Changes for DB Url

--- db/dbOps.js
+++ db/dbOps.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const conStr = 'postgres://loginAdmin:babyDestination@logindb.cgtssx1hr0bo.ap-south-1.rds.amazonaws.com:5432/loginDetails';
+const conStr = 'postgres://loginAdmin@logindb:babyDestination1@logindb.postgres.database.azure.com:5432/logindetails';
 
 const client = new Client (conStr);
 async function connection() {
